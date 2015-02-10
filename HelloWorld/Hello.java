3b2f0c143105110e52b051bddf4ac95707058c0f
MYeagle <yeagle24@gmail.com>
2015-02-10 16:09:05 -0500

    Start of Program
    Simple start of a Hello World Program

---
+++ HelloWorld/Hello.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Hello here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hello
+{
+    private String Message = "";
+    public Hello()
+    {
+        Message = "Hello";
+    }
+    
+    public void changeMessage(String str)
+    {
+        Message = str;
+    }
+    
+}
